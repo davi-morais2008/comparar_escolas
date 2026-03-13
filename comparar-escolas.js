0f25a9a7397e71aeafc18b84f9455cbda1c4ce41
Davi Morais <davi.a.morais@aluno.senai.br>
2026-03-13 09:00:54 -0300

    MOD: Finalização da aula da manhã

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -31,23 +31,24 @@ class Senai {
 
 
 /* ===== OBTER ELEMENTOS ===== */
-const cidadeA = document.querySelector('.unidadeA__cidade');
-const codigoA = document.querySelector('.unidadeA__codigo');
-const anoA = document.querySelector('.unidadeA__ano');
-const cursosA = document.querySelector('.unidadeA__cursos');
+const cidadeA = document.querySelector('input.unidadeA__cidade').value;
+const codigoA = document.querySelector('.unidadeA__codigo').value;
+const anoA = document.querySelector('.unidadeA__ano').value;
+const cursosA = document.querySelector('.unidadeA__cursos').value;
 const btnInstanciarA = document.querySelector('.unidadeA__instanciar')
+console.log(btnInstanciarA)
 
-const cidadeB = document.querySelector('.unidadeB__cidade');
-const codigoB = document.querySelector('.unidadeB__codigo');
-const anoB = document.querySelector('.unidadeB__ano');
-const cursosB = document.querySelector('.unidadeB__cursos');
+const cidadeB = document.querySelector('.unidadeB__cidade').value;
+const codigoB = document.querySelector('.unidadeB__codigo').value;
+const anoB = document.querySelector('.unidadeB__ano').value;
+const cursosB = document.querySelector('.unidadeB__cursos').value;
 const btnInstanciarB = document.querySelector('.unidadeB__instanciar')
 
 
 
 
 const mensagem = document.querySelector('.unidade__status p')
-
+mensagem.innerHTML = `${cidadeA} dfd`
 
 
 /* ===== FUNÇÃO DE VALIDAÇÃO ===== */
@@ -56,18 +57,16 @@ if (cidadeA === '' | codigoA === '' | anoA === '' | cursosA === '' | cidadeB ===
 }
 
 /* ===== INSTANCIAR ESCOLA A ===== */
-const instanciarEscolaA = () => {
-   if (btnInstanciarA){
-    btnInstanciarA.addEventListener('click', function(){new Senai(codigoA, cidadeA, anoA, cursosA)})
-  }
-};
+function instanciarA(){
+  new Senai(codigoA, cidadeA, anoA, cursosA)
+}
+btnInstanciarA.addEventListener('click', instanciarA);
 
 /* ===== INSTANCIAR ESCOLA B ===== */
-const instanciarEscolaB = () => {
-  if (btnInstanciarB){
-    btnInstanciarB.addEventListener('click', function(){new Senai(codigoB, cidadeB, anoB, cursosB)})
-  }
-};
+function instanciarB(){
+  new Senai(codigoB, cidadeB, anoB, cursosB)
+}
+btnInstanciarA.addEventListener('click', instanciarB);
 
 /* ===== ABRIR ESCOLA ===== */
 
